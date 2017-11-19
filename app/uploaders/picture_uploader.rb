813d69ba15a2e517e4dc0f3048f253b2eb908c09
yuh <yuh.pen3@gmail.com>
2017-11-19 09:39:25 +0000

    for heroku

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
