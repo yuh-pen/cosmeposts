eb50731f419be1c2d75befcb097264ed8c62afc1
yuh <yuh.pen3@gmail.com>
2017-11-22 17:01:08 +0000

    aws s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,8 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
         :provider              => 'AWS',
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY_ID'],
+        :aws_access_key_id     => ENV['ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['SECRET_ACCESS_KEY'],
         :region                => 'us-east-1',
     }
 
