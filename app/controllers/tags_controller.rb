6522d47524f16c609b74cb7b6f451f16a619622f
yuh <yuh.pen3@gmail.com>
2017-11-25 11:15:56 +0000

    tag#show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
-    @tagname = params[:format]
-    @cosme_pictures = @user.find_related_tag("マキシマイザー")
+    @tag = params[:format]
+    @cosme_pictures = CosmePicture.tagged_with(params[:format])
   end
 end
