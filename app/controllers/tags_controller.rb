4b297b3e869091dd27fc3b44439e8515c592e4a1
yuh <yuh.pen3@gmail.com>
2017-11-25 15:39:13 +0000

    _cosme_pictures.html.erb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
     @tag = params[:id]
-    @cosme_pictures = CosmePicture.tagged_with(params[:id])
+    @cosme_pictures = CosmePicture.tagged_with(params[:id]).page(params[:page])
   end
 end
